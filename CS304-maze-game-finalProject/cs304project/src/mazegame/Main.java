5bf05fc6da8ea15723c5fc2cb00cecc937af3f1a
MostafaAhmed-tefa <mostafaahmed49k@gmail.com>
2024-12-17 23:06:03 +0200

    Add files via upload

---
+++ CS304-maze-game-finalProject/cs304project/src/mazegame/Main.java
@@ -0,0 +1,13 @@
+package mazegame;
+
+import mazegame.scenes.MainMenuScene;
+import mazegame.scenes.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.start();
+
+    }
+
+}
